a0f78b43bec1565088ce13fc3b54c6e1687d9b1e
stepan <ya.simonov-stepan@yandex.ru>
2021-03-17 23:54:25 +0500

    The user's connection to the event, the appearance of the event

--- Gemfile
+++ Gemfile
@@ -12,8 +12,3 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
 
-group :development do
-  gem 'web-console', '>= 3.3.0'
-end
-
-gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,62 @@
 module ApplicationHelper
+  def user_avatar(user)
+    if user.avatar?
+      user.avatar.url
+    else
+      asset_pack_path('media/images/user.png')
+    end
+  end
+
+  def user_avatar_thumb(user)
+    if user.avatar.file.present?
+      user.avatar.thumb.url
+    else
+      asset_pack_path('media/images/user.png')
+    end
+  end
+
+  def event_photo(event)
+    photos = event.photos.persisted
+
+    if photos.any?
+      photos.sample.photo.url
+    else
+      asset_pack_path('media/images/event.jpg')
+    end
+  end
+
+  def event_thumb(event)
+    photos = event.photos.persisted
+
+    if photos.any?
+      photos.sample.photo.thumb.url
+    else
+      asset_pack_path('media/images/event_thumb.jpg')
+    end
+  end
+
+  def fa_icon(icon_class)
+    content_tag 'span', '', class: "bi bi-#{icon_class}"
+  end
+
+  def bootstrap_flash(opts = {})
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} alert-dismissible fade show", role: 'alert') do
+        concat content_tag(:button, '', class: 'btn-close', data: { 'bs-dismiss': 'alert' })
+        concat message
+      end)
+    end
+    nil
+  end
+
+  private
+
+  def bootstrap_class_for(flash_type)
+    {
+      success: 'alert-success',
+      error: 'alert-danger',
+      alert: 'alert-warning',
+      notice: 'alert-success'
+    }.stringify_keys[flash_type.to_s] || flash_type.to_s
+  end
 end
