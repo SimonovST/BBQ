66b8eac42abe7c9d692bf90e09afba0a736da792
stepan <ya.simonov-stepan@yandex.ru>
2021-03-18 17:21:59 +0500

    Add connection between User commit event and subscription, visual display, recording from the app

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,9 +13,16 @@ class User < ApplicationRecord
   # validates :email, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
   before_validation :set_name, on: :create
 
+  after_commit :link_subscriptions, on: :create
+
   private
 
   def set_name
     self.name = "Товарисч №#{rand(781)}" if self.name.blank?
+  end
+
+  def link_subscriptions
+    Subscription.where(user_id: nil, user_email: self.email)
+      .update_all(user_id: self.id)
   end
 end
