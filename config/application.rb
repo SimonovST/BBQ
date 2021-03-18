673600a9a5702c86fb5f5ee31c1942da1b2f4bf4
stepan <ya.simonov-stepan@yandex.ru>
2021-03-18 21:00:31 +0500

    set up the translation

--- config/application.rb
+++ config/application.rb
@@ -28,8 +28,15 @@ module Bbq1
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.i18n.default_locale = :ru
+
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.available_locales = [:ru]
+    config.i18n.default_locale = :ru
+
+    config.time_zone = 'Moscow'
   end
 end
