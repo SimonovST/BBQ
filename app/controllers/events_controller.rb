b765035a569952a134196cc9959b5572c859fa36
stepan <ya.simonov-stepan@yandex.ru>
2021-03-24 01:50:58 +0500

    Edit

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: [:edit, :update, :destroy, :show]
+  # before_action :set_event, only: [:edit, :update, :destroy, :show]
 
   before_action :authenticate_user!, except: [:show, :index]
   before_action :set_event, only: [:show]
